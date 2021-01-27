fe1229d12cf0f1846be946a404fcd5a1b0b1f027
DOTGo-web <info@dotgo.xyz>
2021-01-27 15:10:26 +0530

    code uploading

---
+++ wp-admin/admin-ajaxd7fc.php
@@ -0,0 +1,15 @@
+<HTML>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from www.linksture.com/wp-admin/admin-ajax.php?action=process_simple_like&nonce=29877b1280&post_id=6348&disabled=true&is_comment=0 by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 27 Jan 2021 07:43:23 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<HEAD>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html;charset=UTF-8"><META HTTP-EQUIV="Refresh" CONTENT="0; URL=../blog/beginners-guide-to-create-a-woocommerce-store-in-5-steps/index.html"><TITLE>Page has moved</TITLE>
+</HEAD>
+<BODY>
+<A HREF="../blog/beginners-guide-to-create-a-woocommerce-store-in-5-steps/index.html"><h3>Click here...</h3></A>
+</BODY>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from www.linksture.com/wp-admin/admin-ajax.php?action=process_simple_like&nonce=29877b1280&post_id=6348&disabled=true&is_comment=0 by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 27 Jan 2021 07:43:23 GMT -->
+</HTML>
